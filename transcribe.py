fcc963e716343bb1215d966635693cdc886469fe
dsam87 <deleonsam87@gmail.com>
2024-06-30 15:38:15 -0500

    i/o transcription

--- transcribe.py
+++ transcribe.py
@@ -1,6 +1,18 @@
 import whisper
+from datetime import datetime
+
+# Get current time
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+# Open file  
+text_output = open("text-files/transcribe.txt", "a")
 
 # Read audio files from "audio-files" dir 
 model = whisper.load_model("base")
 result = model.transcribe("audio-files/audio.mp3")
-print(result["text"])
+
+# Append to file
+text_output.write(f"\n{current_time}\n")
+text_output.write(result["text"] + "\n")
+text_output.close()
