18da17aac64d078f95be1861ddc5d652dbb5224d
dsam87 <deleonsam87@gmail.com>
2024-06-30 15:34:39 -0500

    Create transcribe

--- transcribe.py
+++ transcribe.py
@@ -1,6 +1,6 @@
 import whisper
 
+# Read audio files from "audio-files" dir 
 model = whisper.load_model("base")
-result = model.transcribe("audio.mp3")
+result = model.transcribe("audio-files/audio.mp3")
 print(result["text"])
-
